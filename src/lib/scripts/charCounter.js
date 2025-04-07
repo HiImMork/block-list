aa9d47dd9e437ad85677472ad74ca71346ec2616
hiimmork <main.flaymingo41@gmail.com>
2025-04-07 15:29:57 +0800

    - Redone `charCounter.js` for the nth time too (I also lost count!). Now it's more readable and easier to maintain with more descriptive variable and function names. - Changed initial value of charCounterMessage in `index.html`. - Converted `main section .controls` from flexbox to grid (1x6). - Added a dedicated `main #charCounter.controls` to make it look more applealing.

--- src/lib/scripts/charCounter.js
+++ src/lib/scripts/charCounter.js
@@ -1,13 +1,16 @@
-const [msgEl, countEl, inputEl, clearBtn] = ["#charCounter .message", "#charCounter #charCount", "#charCounter #charCounterInput", "#charCounter .clear"].map(s => document.querySelector(s));
-
-inputEl.addEventListener("input", e => {
-  countEl.textContent = `Characters: ${e.target.value.length}`;
-  msgEl.textContent = "User is typing something...";
-  setTimeout(() => msgEl.textContent = "User has stopped typing. Waiting for input...", 1000);
+const charCountmessage = document.querySelector("#charCounter .message");
+const charCount = document.querySelector("#charCounter #charCount");
+const inputString = document.querySelector("#charCounter #charCounterInput");
+inputString.addEventListener("input", () => {
+  charCount.textContent = "Characters: " + inputString.value.length.toString();
+  
+  charCountmessage.textContent = "User is typing...";
+  setTimeout(() => {
+    charCountmessage.textContent = "Waiting for user input..."
+  }, 1000);
 });
 
-clearBtn.addEventListener("click", () => {
-  inputEl.value = "";
-  countEl.textContent = "Characters: ";
-  msgEl.textContent = "Input has been cleared!";
+const clearInputBtn = document.querySelector("#charCounter button.clear.danger").addEventListener("click", () => {
+  inputString.value = "";
+  charCountmessage.textContent = "Input has been cleared. Waiting for user input...";
 });
